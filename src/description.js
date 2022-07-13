21546578e1da316db8e724a7f3f5b02c2b5c1fab
arieri <ariericyron@gmail.com>
2022-07-13 17:58:43 +0300

    added a component

---
+++ src/description.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+function Description(){
+    return <div>Penny Sansevieri, CEO and founder of Author Marketing Experts, Inc. (AME) and Adjunct Professor at NYU, is a best-selling author and internationally recognized book marketing and media relations expert. Her company is one of the leaders in the publishing industry and has developed some of the most cutting-edge book marketing campaigns. To learn more about Penny’s books or her promotional services, visit Author Marketing Experts (AME).</div>
+}
+
+export default Description;
